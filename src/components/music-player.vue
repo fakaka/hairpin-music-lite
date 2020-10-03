668f1469e97cd8857ee3a8b25a55ba6a60c4bb2f
hairpin <vsmj012@163.com>
2020-10-03 17:25:24 +0800

    增加 下首提醒

--- src/components/music-player.vue
+++ src/components/music-player.vue
@@ -149,11 +149,34 @@ export default {
         },
         next() {
             if (this.songReady) {
+                this.notify(this.nextSong)
                 this.startSong(this.nextSong)
             }
         },
         end() {
             this.next()
+        },
+        notify(song) {
+            const NotificationInstance = Notification || window.Notification
+            if (NotificationInstance) {
+                const permissionNow = NotificationInstance.permission
+                if (permissionNow === 'granted') {
+                    //允许通知
+                    new Notification('YOYO 音乐', {
+                        body: song.name + ' - ' + song.artistsText,
+                        tag: 'next',
+                        renotify: true
+                    })
+                } else if (permissionNow === 'denied') {
+                    console.log('用户拒绝了你!!!')
+                } else {
+                    NotificationInstance.requestPermission(function(PERMISSION) {
+                        if (PERMISSION !== 'granted') {
+                            console.log('用户无情残忍的拒绝了你!!!')
+                        }
+                    })
+                }
+            }
         },
         onProgressChange(percent) {
             this.audio.currentTime = this.currentSong.durationSecond * percent
